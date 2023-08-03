4441d828de9270de1c3a0e51b2b35b71a5b6bdf0
Maxime Authié <authie.maxime@orange.fr>
2023-08-03 20:47:32 +0200

    Ajout bouton "nouvel article"

--- pages/managerApp/articles/index.vue
+++ pages/managerApp/articles/index.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="admin_content">
         <h1>Gestion des articles</h1>
+        
+        <nuxt-link to="/managerApp/articles/newArticle" class="article_item_icon_link">
+            <button class="admin_button admin_button_main">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+
+                Nouvel article
+            </button>
+        </nuxt-link>
+            
+        
         <ManagerArticlesListComponent></ManagerArticlesListComponent>
     </div>
 </template>
@@ -18,5 +30,13 @@
         font-size: 2.2em;
         color: #4B453F;
         text-shadow: none;
+     }
+
+    .article_item_icon_link {
+        color: #FFFFFF;
+    }
+
+    .admin_button svg {
+        width: 3vh;
     }
 </style>
