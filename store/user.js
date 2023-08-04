e5a9fbf44fb02ab1985c081dfddbb187374576a0
Maxime Authié <authie.maxime@orange.fr>
2023-08-04 19:29:51 +0200

    Créations store et méthode getAllUsers()

---
+++ store/user.js
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia';
+
+export const useUsersStore = defineStore('users', {
+  state: () => ({
+    users: [],
+  }),
+  actions: {
+    async getAllUsers() {
+      console.log("getAllUsers lancé");
+        try {
+            await $fetch('https://127.0.0.1:8000/api/user/all', {
+                method:'GET',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                }
+            }).then(response => {
+              const usersList = response;
+              this.users = usersList;
+            })
+            
+        } catch (error) {
+            console.error(error.message);
+        }
+        
+    }
+  },
+});
