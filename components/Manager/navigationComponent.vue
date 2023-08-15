40d6f93163fb9cdb4d31867d0c54c55089570720
Maxime Authié <authie.maxime@orange.fr>
2023-08-15 18:16:04 +0200

    gestion de la déconnexion

--- components/Manager/navigationComponent.vue
+++ components/Manager/navigationComponent.vue
@@ -62,17 +62,19 @@
                 </nuxt-link>
             </li>
             <li>
-                <nuxt-link to="/" :class="route=='/' ? 'navigation_bar_list_link_selected' : 'navigation_bar_list_link'">
+                <div @click="logOut" class="navigation_bar_list_link">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clip-rule="evenodd" />
                     </svg>
                 Déconnexion
-                </nuxt-link>
+                </div>
             </li>
         </ul>
     </div>
 </template>
 <script>
+    import { useUsersStore } from "@/store/user";
+
     export default {
         props: {
             route: {
@@ -80,6 +82,13 @@
                 require: true
             }
         },
+        methods: {
+            logOut() {
+                const store = useUsersStore();
+                store.token = '';
+                this.$router.push('/managerApp/logIn'); 
+            }
+        }
     }
 </script>
 <style scoped>
