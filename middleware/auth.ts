98d9a211962679143d8c4c4b27b2b6793a3ee13b
Maxime Authié <authie.maxime@orange.fr>
2023-08-15 18:18:01 +0200

    Ajout des cas de redirect vers logIn()

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,7 +3,7 @@ import { useUsersStore } from "@/store/user";
 export default defineNuxtRouteMiddleware((to, from) => {
     const store = useUsersStore();
     
-    if (store.token == '') {
+    if (store.token == '' || store.token == 'expired-token' || store.token == 'invalid-token' || store.token == 'expired-sessio') {
         return navigateTo("/managerApp/logIn");
     }
     
