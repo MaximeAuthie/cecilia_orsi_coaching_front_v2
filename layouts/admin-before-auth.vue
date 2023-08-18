dc85b17ff982a80c6568a982442771be1ac7901b
Maxime Authié <authie.maxime@orange.fr>
2023-08-18 21:29:52 +0200

    Ajout <head> pour robots

--- layouts/admin-before-auth.vue
+++ layouts/admin-before-auth.vue
@@ -2,3 +2,18 @@
     <ManagerHeaderComponent></ManagerHeaderComponent>
     <slot/>
 </template>
+
+<script>
+    export default {
+        mounted() {
+            //? Renseigner les balises HTML de <head> pour bloquer le SEO
+            useHead({
+                title: 'Cécilia Orsi Coaching - Espace administrateur',
+                meta: [
+                    {name:'robots', content:'noindex, nofollow'},
+                ],
+                link: [{rel: 'icon', href: './assets/images/icone_tree.png'}]
+            })
+        }
+    }
+</script>
