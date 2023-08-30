e77256cff0a58d8e78cae7a7022c8909bb57dcbd
Maxime Authié <authie.maxime@orange.fr>
2023-08-30 20:34:33 +0200

    Installation NuxtImage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ["~/assets/style/main.css"],
-  modules: ["@pinia/nuxt"],
-  build: {
-    transpile: ['pinia'],
-  }
+    devtools: { enabled: true },
+    css: ["~/assets/style/main.css"],
+    modules: [
+        "@pinia/nuxt",
+        '@nuxt/image',
+    ],
+    build: {
+        transpile: ['pinia'],
+    },
+    runtimeConfig: {
+        public: {
+            serverUrl: 'https://www.maximeauthie.fr/'
+        }
+    }
 })
