31114671dff761b2e2046e279fac81b8254fa1db
Maxime Authié <authie.maxime@orange.fr>
2023-08-30 20:38:47 +0200

    Ajout logique pour champs à saisie obligatoire

--- pages/managerApp/pages.vue
+++ pages/managerApp/pages.vue
@@ -16,18 +16,20 @@
         </div>
        <div v-if="selectedPageId != 0" class="admin_content_form">
             <div class="admin_content_form_bloc">
-                <label for="status" class="admin_label">URL de la bannière : </label>
-                <input v-model="selectedPageData.banner_url_page" type="text" class="admin_input_form">
+                <label for="status" class="admin_label">URL de la bannière *: </label>
+                <input v-model="selectedPageData.banner_url_page" @keyup="checkInputKeyUp" type="text" :class=" errorMessages.bannerUrlEmpty != ''  ? 'bad_admin_input_form' : 'admin_input_form'">
+                <span class="admin_error_message_form">{{ errorMessages.bannerUrlEmpty }}</span>
             </div>
             <div class="admin_content_form_bloc">
-                <label for="status" class="admin_label">Texte de la bannière: </label>
+                <label for="status" class="admin_label">Texte de la bannière: *</label>
                 <div v-for="bannerText in selectedPageData.BannerTextsList" class="admin_content_form_bloc_banner_text">
-                    <input v-model="bannerText.content_banner_text" type="text" class="admin_input_form">
+                    <input v-model="bannerText.content_banner_text" @keyup="checkInputKeyUp" type="text" class="admin_input_form">
                     <svg @click="deleteBannerText(bannerText.id)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" >
                         <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
                     </svg>
                 </div>
-                <span @click="addBannerText" class="admin_content_form_bloc_add_banner_text">Ajouter un texte de bannière</span>
+                <div @click="addBannerText" class="admin_content_form_bloc_add_banner_text">Ajouter un texte de bannière</div>
+                <div class="admin_error_message_form">{{ errorMessages.bannerTextEmpty }}</div>
             </div>
             <div class="admin_content_form_bloc">
                 <label for="status" class="admin_label">URL de l'image 1 : </label>
@@ -37,8 +39,9 @@
                 <label for="status" class="admin_label">URL de l'image 2 : </label>
                 <input v-model="selectedPageData.img2_url_page" type="text" class="admin_input_form">
             </div>
+            * champs obligatoires
             <div class="admin_content_filters_message">
-                <span v-if="formErrorMessage" class="admin_content_filters_message_error">{{ formErrorMessage }}</span>
+                <span v-if="errorMessages.form" class="admin_content_filters_message_error">{{ formErrorMessage }}</span>
                 <span v-if="formSuccessMessage" class="admin_content_filters_message_success">{{ formSuccessMessage }}</span>
             </div>
             <div class="admin_content_form_buttons_container">
@@ -59,7 +62,12 @@
                 selectedPageId: 0,
                 selectedPageData: [],
                 formSuccessMessage: '',
-                formErrorMessage: ''
+                formErrorMessage: '',
+                errorMessages: {
+                    form:                '',
+                    bannerUrlEmpty:      '',
+                    bannerTextEmpty:     '',
+                },
             }
         },
         methods: {
@@ -88,7 +96,7 @@
             displayPageData(event) {
                 const index = event.target.selectedIndex;
                 this.selectedPageData = this.pages[index];
-                this.formErrorMessage ='';
+                this.errorMessages.form ='';
                 this.formSuccessMessage = '';
             },
             addBannerText() {
@@ -97,25 +105,60 @@
                     content_banner_text: ''
                 })
             },
+            checkInputBeforeSubmit() {
+                if (this.selectedPageData.banner_url_page == '') {
+                    this.errorMessages.bannerUrlEmpty   = "Veuillez renseigner une URL pour la bannière de la page";
+                    this.errorMessages.form             = "Veuillez remplir tous les champs obligatoires du formulaire";
+                }
+                if (this.selectedPageData.BannerTextsList == '') {
+                    this.errorMessages.bannerTextEmpty  = "Veuillez saisir au moins un texte pour la bannière de la page";
+                    this.errorMessages.form             = "Veuillez remplir tous les champs obligatoires du formulaire"
+                }
+            },
+            checkErrorMessages() {
+                if (
+                    this.errorMessages.form                 != '' 
+                    || this.errorMessages.bannerUrlEmpty    != '' 
+                    || this.errorMessages.bannerTextEmpty   != '' 
+                ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },
+            checkInputKeyUp() {
+                if (this.selectedPageData.banner_url_page != '') {
+                    this.errorMessages.bannerUrlEmpty   = "";
+                }
+                if (this.selectedPageData.BannerTextsList != '') {
+                    this.errorMessages.bannerTextEmpty    = "";
+                }
+            },
             async updatePage() {
 
                 const pageStore = usePagesStore();
                 
                 //? Rénitialiser les messages d'erreur
-                this.formErrorMessage ='';
+                this.errorMessages.form ='';
                 this.formSuccessMessage = '';
-                
-                //? Appel de la méthode updatePage() du composable usePage
-                const { updatePage }    = usePage();
-                const response          = await updatePage(this.selectedPageData);
-                const body              = await response.json();
 
-                //? En fonction du statut de la réponse, afficher le message d'erreur ou de succès correspondant
-                if (response.status == 200) {
-                    this.formSuccessMessage = body.message;
-                    pageStore.getAllPages();
-                } else {
-                    this.formErrorMessage ='Une erreur est survenue. Veuillez réessayer plus tard';
+                //? Vérifier si les champs nécessaires sont bien renseignés
+                this.checkInputBeforeSubmit();
+                
+
+                if (this.checkErrorMessages()) {
+                    //? Appel de la méthode updatePage() du composable usePage
+                    const { updatePage }    = usePage();
+                    const response          = await updatePage(this.selectedPageData);
+                    const body              = await response.json();
+
+                    //? En fonction du statut de la réponse, afficher le message d'erreur ou de succès correspondant
+                    if (response.status == 200) {
+                        this.formSuccessMessage = body.message;
+                        pageStore.getAllPages();
+                    } else {
+                        this.errorMessages.form ='Une erreur est survenue. Veuillez réessayer plus tard';
+                    }
                 }
             },
             deleteBannerText(id) {
