7b0ea19cc7e1c98c2f07785240000997b8db2eca
Maxime Authié <authie.maxime@orange.fr>
2023-08-30 20:36:06 +0200

    Création component loader

---
+++ components/loaderComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="waiting_div">
+        <div class="waiting_div_logo">
+            <img src="~/assets/images/logo_loader.png" alt="logo">
+        </div>
+        <h2>Cécilia Orsi Coaching</h2>
+        <div class="waiting_div_loader">
+            <p>Chargement en cours...</p>
+        </div>
+    </div>
+</template>
