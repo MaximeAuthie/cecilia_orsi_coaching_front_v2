beec0c480b36765920a2e51b6c7ebf1cff0eac8c
Maxime Authié <authie.maxime@orange.fr>
2023-09-04 18:41:20 +0200

    Erreur syntaxe useVisitsStore

--- store/visit.js
+++ store/visit.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 
-export const useVisitStore = defineStore('visit', {
+export const useVisitsStore = defineStore('visit', {
     state: () => ({
         stats:      {}
     }),
