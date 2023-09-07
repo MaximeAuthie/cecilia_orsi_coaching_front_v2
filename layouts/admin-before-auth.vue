2fb48c1309ee7dae3ea8e56c80a88eea4688962b
Maxime Authié <authie.maxime@orange.fr>
2023-09-07 18:59:34 +0200

    Bug logo onglet

--- layouts/admin-before-auth.vue
+++ layouts/admin-before-auth.vue
@@ -9,13 +9,17 @@
     export default {
         mounted() {
 
+            //? Récupérer l'url de l'icone d'onglet
+            const config    = useRuntimeConfig();
+            const iconUrl = config.public.metaLinkIconUrl;
+
             //? Renseigner les balises HTML de <head> pour bloquer le SEO
             useHead({
                 title: 'Cécilia Orsi Coaching - Connexion',
                 meta: [
                     {name:'robots', content:'noindex, nofollow'},
                 ],
-                link: [{rel: 'icon', href: '/_nuxt/assets/images/icone_tree.png'}]
+                link: [{rel: 'icon', href: iconUrl}]
             })
         }
     }
