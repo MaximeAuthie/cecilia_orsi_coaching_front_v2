dfbe50b606a5362b7e052db97160492d23b6e730
Maxime Authié <authie.maxime@orange.fr>
2023-11-10 17:04:53 +0100

    Modification NDD backend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     },
     runtimeConfig: {
         public: {
-            serverUrl: 'https://www.maximeauthie.fr/',
+            serverUrl: 'https://www.cecilia-orsi.fr/',
             metaLinkIconUrl : 'https://www.hebergeur-image.com/upload/90.76.204.82-64f5f7498ee28.png',
             phoneNumber: '+33756815087'
         }
